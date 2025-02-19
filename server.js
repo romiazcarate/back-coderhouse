4ba61c886d8cb47f7346dfa37a9afa47d43d66af
romiazcarate <romiazcarate05@gmail.com>
2025-02-19 16:30:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,45 @@
 import express from 'express'
 import path from 'path'
 import { createServer } from 'http'
-import productsRouter from './routes/productsRouter.js'
-import cartsRouter from './routes/cartsRouter.js'
 import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
-import { getProducts } from './routes/productsRouter.js'
 
+// Importamos el router de productos como función factory
+import createProductsRouter, { getProducts } from './routes/productsRouter.js';
+import cartsRouter from './routes/cartsRouter.js'
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
-
 const webSocketServer = new Server(server)
 
-// Handlebars
+// Configuracion de Handlebars
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', path.join(path.resolve(), 'views'))
 
-// Middleware
+// Middlewares para parsear JSON, datos URL-encoded y servir archivos estáticos
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
 
-
 // Rutas
+const productsRouter = createProductsRouter(webSocketServer);
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
+// Ruta principal que renderiza la vista "home" y envía los productos existentes
 app.get('/', async (req,res)=> {
-	let products = await getProducts()
-    	console.log(products)
-    	res.render('home', { products})
+    const products = await getProducts();
+    res.render('home', { title: 'Mi Tienda', products });
 })
 
 // Websockets
-
 webSocketServer.on('connection', (socket)=> {
     console.log('Usuario Conectado')
-
-    socket.emit('updateProducts', products)
-
-    socket.on('newProduct', (product)=>{
-        addProduct(product)
-        webSocketServer.emit('updateProducts', getProducts())
-    })
-
-    socket.on('deleteProducts', (id)=>{
-        removeProduct(id)
-        webSocketServer.emit('updateProducts', getProducts())
-    })
-    
 })
 
 
 const PORT = 8080
-
 server.listen(PORT, () => {
     console.log(` Servidor ok en http://localhost:${PORT}`)
 })
